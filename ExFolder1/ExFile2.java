9340ae1660a6d2b2ab2dc43f1aa4eb4c60407300
unknown <2018hw86003@wilp.bits-pilani.ac.in>
2022-06-16 13:26:33 +0530

    updating ExFile2

--- ExFolder1/ExFile2.java
+++ ExFolder1/ExFile2.java
@@ -1,15 +1,51 @@
-public class SumNatural {
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
-    public static void main(String[] args) {
+class Main {
+  public static Set<String> getPermutation(String str) {
 
-        int num = 100, sum = 0;
+    // create a set to avoid duplicate permutation
+    Set<String> permutations = new HashSet<String>();
 
-        for(int i = 1; i <= num; ++i)
-        {
-            // sum = sum + i;
-            sum += i;
-        }
+    // check if string is null
+    if (str == null) {
+      return null;
+    } else if (str.length() == 0) {
+      // terminating condition for recursion
+      permutations.add("");
+      return permutations;
+    }
 
-        System.out.println("Sum = " + sum);
+    // get the first character
+    char first = str.charAt(0);
+
+    // get the remaining substring
+    String sub = str.substring(1);
+
+    // make recursive call to getPermutation()
+    Set<String> words = getPermutation(sub);
+
+    // access each element from words
+    for (String strNew : words) {
+      for (int i = 0;i<=strNew.length();i++){
+
+        // insert the permutation to the set
+        permutations.add(strNew.substring(0, i) + first + strNew.substring(i));
+      }
+    }
+    return permutations;
+  }
+
+  public static void main(String[] args) {
+
+    // create an object of scanner class
+    Scanner input = new Scanner(System.in);
+
+    // take input from users
+    System.out.print("Enter the string: ");
+    String data = input.nextLine();
+    System.out.println("Permutations of " + data + ": \n" + getPermutation(data));
     }
 }
+
